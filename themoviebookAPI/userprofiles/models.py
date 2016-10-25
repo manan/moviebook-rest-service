5a6413d92477a7823292ea77a69b9b06a547fca8
Manan Mehta <mananmehta@JARVIS.local>
2016-10-25 16:38:57 -0700

    change custom models.User to models.UserProfile for clarity

--- themoviebookAPI/userprofiles/models.py
+++ themoviebookAPI/userprofiles/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Profile(models.Model):
+class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank = True, null = True)
     birth_date = models.DateField(blank = True, null = True)
