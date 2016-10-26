1956f54245fcf581ce39679e7fcf7b965f2268d0
Manan Mehta <mananmehta@JARVIS.local>
2016-10-25 17:26:22 -0700

    add models.Follow to app userprofiles

--- themoviebookAPI/userprofiles/admin.py
+++ themoviebookAPI/userprofiles/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import UserProfile
-
+from .models import Follow
 # Register your models here.
 
 admin.site.register(UserProfile)
+admin.site.register(Follow)
--- themoviebookAPI/userprofiles/models.py
+++ themoviebookAPI/userprofiles/models.py
@@ -4,7 +4,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Follow(models.Model):
+    following = models.ForeignKey(User, related_name = "who_follows")
+    follower = models.ForeignKey(User, related_name = "who_is_followed")
+    follow_time = models.DateTimeField(auto_now = True)
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank = True, null = True)
     birth_date = models.DateField(blank = True, null = True)
+    # relationships = models.ManyToManyField(Follow, symmetrical=False, related_name='related_to',
+                                           # null = True, blank = True)
