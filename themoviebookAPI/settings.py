dfce8aff241dbc6cf52333c998d8966d665d9385
Manan Mehta <mehtamanan@icloud.com>
2016-12-26 09:29:30 +0530

    refactor settings as changed directory

--- themoviebookAPI/settings.py
+++ themoviebookAPI/settings.py
@@ -16,6 +16,11 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# then for each subdirectory:
+SOME_DIR = os.path.join(BASE_DIR, 'my_dir')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
