d50d82e2cd7357c859c6025f46f7a729f21016b4
Manan Mehta <mehtamanan@icloud.com>
2017-02-14 18:04:45 -0800

    set debug to True

--- themoviebookAPI/settings.py
+++ themoviebookAPI/settings.py
@@ -125,7 +125,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 try:
     from .local_settings import *
