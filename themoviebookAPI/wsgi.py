211a34419f3855d124aa301b3fa07e6dddafc3d0
Manan Mehta <mehtamanan@icloud.com>
2017-02-15 12:50:50 -0800

    wrap whitenoise over the application to handle profile pictures

--- themoviebookAPI/wsgi.py
+++ themoviebookAPI/wsgi.py
@@ -13,5 +13,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "themoviebookAPI.settings")
 application = get_wsgi_application()
 
-# from whitenoise.django import DjangoWhiteNoise
-# application = DjangoWhiteNoise(application)
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
