1e569881ced358d69a06dfe37ccad8eac942e77a
Manan Mehta <mehtamanan@icloud.com>
2017-03-04 20:27:09 -0800

    add basic views for movies app

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,68 @@
 from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework.authentication import TokenAuthentication
+
+from .serializers import MovieReadSerializer, MovieWriteExtendedSerializer, MovieWriteSimpleSerializer
+from .serializers import GenreSerializer, PersonalitySerializer
+from .models import Movie, Genre, Personality
+
+from rest_framework import generics
 
 # Create your views here.
+
+
+# ['POST']
+class AddMovie(generics.CreateAPIView):
+    """
+    https://themoviebook.herokuapp.com/movies/add/
+
+    Required Keys for POST: ...
+    """
+    model = Movie
+    serializer_class = MovieWriteSimpleSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+
+
+# ['GET']
+class MoviesList(generics.ListAPIView):
+    """
+    https://themoviebook.herokuapp.com/movies/
+    """
+    model = Movie
+    queryset = Movie.objects.all()
+    serializer_class = MovieReadSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (
+        permissions.IsAdminUser,
+    )
+
+
+# ['GET']
+class GenreList(generics.ListAPIView):
+    """
+    https://themoviebook.herokuapp.com/movies/genres/
+    """
+    model = Genre
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (
+        permissions.IsAdminUser,
+    )
+
+
+# ['GET']
+class PersonalityList(generics.ListAPIView):
+    """
+    https://themoviebook.herokuapp.com/movies/personalities/
+    """
+    model = Personality
+    queryset = Personality.objects.all()
+    serializer_class = PersonalitySerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (
+        permissions.IsAdminUser,
+    )
