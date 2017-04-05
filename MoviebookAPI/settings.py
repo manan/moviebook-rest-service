9f955ae285ed6260906e012313c0c940f79cfc3b
Manan Mehta <mehtamanan@icloud.com>
2017-04-04 23:14:35 -0700

    change default permission

--- MoviebookAPI/settings.py
+++ MoviebookAPI/settings.py
@@ -102,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
