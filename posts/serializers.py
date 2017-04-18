a493038dc15351f2ac8be4dc5db8a5f2b4d31a4b
Manan Mehta <mehtamanan@icloud.com>
2017-04-17 23:32:59 -0700

    add owner's profile picture in post

--- posts/serializers.py
+++ posts/serializers.py
@@ -4,8 +4,9 @@ from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
     username = serializers.ReadOnlyField(source='owner.user.username')
+    profile_picture = serializers.ReadOnlyField(source='owner.user.profile.profile_picture')
 
     class Meta:
         model = Post
-        fields = ('owner', 'username', 'movie_title', 'movie_id', 'caption',
+        fields = ('owner', 'username', 'profile_picture', 'movie_title', 'movie_id', 'caption',
                   'upload_date', 'id')
