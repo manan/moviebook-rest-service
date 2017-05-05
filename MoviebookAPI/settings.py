90d05bfb14f42362cc5aa7dd06c0ecf2f579b029
Manan Mehta <mehtamanan@icloud.com>
2017-05-04 23:33:56 -0700

    minor changes

--- MoviebookAPI/settings.py
+++ MoviebookAPI/settings.py
@@ -156,8 +156,8 @@ JWT_AUTH = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_PASSWORD = 'Admin123#'
-EMAIL_HOST_USER = 'mehtamanan@moviebookinc.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
